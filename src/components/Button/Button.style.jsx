b12a97ea1d9ff5234f49994edfeea6cf53be9556
tayor <adenusiadetayo@gmail.com>
2022-11-15 05:56:57 +0100

    style: added navbar styles

--- src/components/Button/Button.style.jsx
+++ src/components/Button/Button.style.jsx
@@ -3,4 +3,17 @@ import styled from "styled-components";
 export const Button = styled.button`
   color: var(--clr-white);
   background: var(--clr-primary);
+  outline: none;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 48px;
+  width: 170px;
+  border-radius: 10px;
+  font-family: var(--font-primary);
+  font-weight: 400;
+  font-size: 1rem;
+  color: var(--clr-white);
+  cursor: pointer;
 `;
--- src/components/Navbar/Navbar.style.jsx
+++ src/components/Navbar/Navbar.style.jsx
@@ -2,4 +2,38 @@ import styled from "styled-components";
 
 export const Header = styled.header`
   width: 100%;
+  height: 110px;
+  .navbar__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    width: 87.5%;
+    margin: 0 auto;
+  }
+  .img__container {
+    width: 233px;
+    height: 36.67px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .navlinks {
+    ul {
+      display: flex;
+
+      li {
+        margin-right: 48px;
+        list-style: none;
+        span {
+          font-family: var(--font-primary);
+          font-weight: 400;
+          font-size: 1.25rem;
+          color: var(--clr-black-2);
+          cursor: pointer;
+        }
+      }
+    }
+  }
 `;
