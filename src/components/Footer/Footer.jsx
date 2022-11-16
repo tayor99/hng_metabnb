8265cf1ca3de3f6869dacc0958ee433c306cde6c
tayor <adenusiadetayo@gmail.com>
2022-11-16 03:28:05 +0100

    feature : added footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,56 @@
 import { Wrapper } from "./Footer.style";
+import logo from "../../Assets/Logo2.svg";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
-  return <Wrapper>Footer</Wrapper>;
+  return (
+    <Wrapper>
+      <div className="footer__container">
+        <div className="company">
+          <div className="company__logo">
+            <img src={logo} alt="company-logo" />
+          </div>
+          <div className="social__links">
+            <FaFacebookF />
+            <FaInstagram />
+            <FaTwitter />
+          </div>
+          <div className="certification">
+            <p> &copy; 2022 Metabnb</p>
+          </div>
+        </div>
+        <div className="footer__links">
+          <div className="community">
+            <h1>Community</h1>
+            <ul>
+              <li>NFT</li>
+              <li>Tokens</li>
+              <li>Landlords</li>
+              <li>Discord</li>
+            </ul>
+          </div>
+          <div className="Places">
+            <h1>Places</h1>
+            <ul>
+              <li>Castle</li>
+              <li>Farms </li>
+              <li>Beach</li>
+              <li>Learn more</li>
+            </ul>
+          </div>
+          <div className="About">
+            <h1>About Us</h1>
+            <ul>
+              <li>Road map</li>
+              <li>Creators </li>
+              <li>Career</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Footer;
--- src/components/Footer/Footer.style.jsx
+++ src/components/Footer/Footer.style.jsx
@@ -2,4 +2,56 @@ import styled from "styled-components";
 
 export const Wrapper = styled.footer`
   width: 100%;
+  background-color: var(--clr-black-4);
+  padding: 63px 0 37px 0;
+  .footer__container {
+    display: flex;
+    justify-content: space-between;
+    width: 84%;
+    margin: 0 auto;
+  }
+  .company {
+    width: 40%;
+  }
+  .social__links {
+    margin-top: 100px;
+    svg {
+      color: var(--clr-white-2);
+      font-size: 1.2rem;
+      margin-right: 28px;
+      cursor: pointer;
+    }
+  }
+  .certification {
+    p {
+      font-family: var(--font-primary);
+      font-weight: 400;
+      font-size: 1rem;
+      color: var(--clr-white-2);
+      margin-top: 40px;
+    }
+  }
+  .footer__links {
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+    h1 {
+      font-family: var(--font-primary);
+      font-weight: 700;
+      font-size: 1.1rem;
+      color: var(--clr-white);
+    }
+    ul {
+      margin-top: 23px;
+    }
+    li {
+      list-style: none;
+      font-family: var(--font-primary);
+      font-weight: 400;
+      font-size: 1rem;
+      color: var(--clr-white-3);
+      margin-bottom: 16px;
+      cursor: pointer;
+    }
+  }
 `;
